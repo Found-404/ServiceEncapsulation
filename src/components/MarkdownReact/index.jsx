babc9803e39c8829a9e59f798c3e88adb7f76100
Found-404 <2247124474@qq.com>
2023-09-12 09:55:51 +0800

    feat: md文件读取

--- src/components/MarkdownReact/index.jsx
+++ src/components/MarkdownReact/index.jsx
@@ -1,53 +1,51 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // 设置高亮样式
-// import { xonokai } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { xonokai } from "react-syntax-highlighter/dist/esm/styles/prism";
+import md from "../../../MD/CustomFormComp.md";
 import "github-markdown-css";
-// const Code = {
-//   code({ node, inline, className, children, ...props }) {
-//     const match = /language-(\w+)/.exec(className || "");
-//     return !inline && match ? (
-//       <SyntaxHighlighter
-//         children={String(children).replace(/\n$/, "")}
-//         style={xonokai}
-//         language={match[1]}
-//         PreTag="div"
-//         {...props}
-//       />
-//     ) : (
-//       <code className={className} {...props}>
-//         {children}
-//       </code>
-//     );
-//   },
-// };
+
+const Code = {
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        children={String(children).replace(/\n$/, "")}
+        style={xonokai}
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      />
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
 const MarkdownReact = () => {
-  const [docmentContent, setDocmentContent] = useState("");
-  const content = `This is some JavaScript code:
-  ~~~js
-  console.log('Hello world!')
-  ~~~
-
-  ### styles和classNames属性
-  | 名称 | 说明         | 版本  |
-  | ---- | ------------ | ----- |
-  | icon | 设置图标元素 | 5.5.0 |
-  | icon | 设置图标元素 | 5.5.0 |
-  | icon | 设置图标元素 | 5.5.0 |
-  | icon | 设置图标元素 | 5.5.0 |
-
-
-  `;
+  // console.log("pathStr", fileDisplay("../../../MD"));
+  const [currentArticle, setCurrentArticle] = useState({
+    url: "",
+    content: "",
+  });
+  // 更改当前文档
+  const changeCurrentArticle = async (url) => {
+    const res = await fetch(url);
+    const content = await res.text();
+    setCurrentArticle({ ...currentArticle, content, url });
+  };
   useEffect(() => {
-    setDocmentContent(content);
+    changeCurrentArticle(md);
   }, []);
+
   return (
     <div className="markdown-body" style={{ padding: "30px" }}>
       <ReactMarkdown
-        children={docmentContent}
-        // components={Code}
+        children={currentArticle.content}
+        components={Code}
         remarkPlugins={[remarkGfm]}
       />
     </div>
